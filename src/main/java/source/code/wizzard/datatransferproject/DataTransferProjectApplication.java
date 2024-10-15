b2a0ba59a095c672f5e904c51e8a6d1ee9ee64a8
SourceCodeWizard <chalassproduction@gmail.com>
2024-10-16 01:19:57 +1200

    first commit

---
+++ src/main/java/source/code/wizzard/datatransferproject/DataTransferProjectApplication.java
@@ -0,0 +1,13 @@
+package source.code.wizzard.datatransferproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataTransferProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataTransferProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/source/code/wizzard/datatransferproject/DataTransferProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package source.code.wizzard.datatransferproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTransferProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
